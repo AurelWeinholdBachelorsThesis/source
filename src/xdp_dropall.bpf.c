ebb7678d2ebe00c6dd99b0d095ad752cce611748
Aurel Weinhold <aurel@weinhold.org>
2022-06-20 20:34:04 +0200

    Pass all and get data and data_end pointer

--- src/xdp_dropall.bpf.c
+++ src/xdp_dropall.bpf.c
@@ -6,5 +6,8 @@ char LICENSE[] SEC("license") = "GPL";
 SEC("xdp")
 int drop_port(struct xdp_md *ctx)
 {
-	return XDP_DROP;
+	void *data = (void *)(long)ctx->data;
+	void *data_end = (void *)(long)ctx->data_end;
+
+	return XDP_PASS;
 }
